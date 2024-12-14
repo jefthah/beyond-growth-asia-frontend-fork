0fe37fd2df8275c21731ec64bc55ab66c13cb771
heydaremir <2110511150@mahasiswa.upnvj.ac.id>
2024-12-14 22:41:58 +0700

    updating vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/beyond-growth-asia-frontend/', // Set this to your repository name
-});
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
